04c010069f71575ad87a34143af276c9c072584a
shb <2269629396@qq.com>
2019-06-27 15:12:23 +0800

    编写页面功能

--- web-admin/src/views/login/index.vue
+++ web-admin/src/views/login/index.vue
@@ -43,32 +43,12 @@
             @blur="capsTooltip = false"
             @keyup.enter.native="handleLogin"
           />
-          <!--<span class="show-pwd" @click="showPwd">-->
-          <!--<svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />-->
-          <!--</span>-->
         </el-form-item>
       </el-tooltip>
 
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">
         {{ $t('login.logIn') }}
       </el-button>
-
-      <!--<div style="position:relative">-->
-      <!--<div class="tips">-->
-      <!--<span>{{ $t('login.username') }} : admin</span>-->
-      <!--<span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>-->
-      <!--</div>-->
-      <!--<div class="tips">-->
-      <!--<span style="margin-right:18px;">-->
-      <!--{{ $t('login.username') }} : editor-->
-      <!--</span>-->
-      <!--<span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>-->
-      <!--</div>-->
-
-      <!--<el-button class="thirdparty-button" type="primary" @click="showDialog=true">-->
-      <!--{{ $t('login.thirdparty') }}-->
-      <!--</el-button>-->
-      <!--</div>-->
     </el-form>
 
     <el-dialog :title="$t('login.thirdparty')" :visible.sync="showDialog">
